ca4f8d23e71d134f44f90fa45b1cd6e212c9bedd
Annie <annie.tang.25@dartmouth.edu>
2022-06-29 21:07:33 -0400

    added home and about me sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 50) {
+        header.classList.add('scroll-header');
+    }
+    else {
+        header.classList.remove('scroll-header');
+    }
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
 
