8d87ff5b2f80dc14d8668ea0d21411804b0a1515
Annie <annie.tang.25@dartmouth.edu>
2022-07-01 16:55:31 -0400

    completed first draft

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # annieetang.github.io
+
+Wonderful sources:
+
+* Bedimcode
+* [Icons](https://boxicons.com/)
+* [Fonts](https://fonts.google.com/)
+* [Mixitup](https://www.kunkalabs.com/mixitup/)
+* [Swiper](https://swiperjs.com/)
+* [ScrollReveal](https://scrollrevealjs.org/)
+* [Dopely](https://colors.dopely.top/color-pedia)
--- assets/js/main.js
+++ assets/js/main.js
@@ -131,4 +131,14 @@ themeButton.addEventListener('click', () => {
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true,
+})
 
+sr.reveal(`.home__data`)
+sr.reveal(`.home__handle`, {delay: 200})
+sr.reveal(`.home__social, .home__scroll`, {delay: 200, origin: 'bottom'})
