b2eab1b8053cad82823b7629a1c6eaf53a85f000
Annie Tang <98965493+annieetang@users.noreply.github.com>
2022-09-11 22:29:51 -0400

    added modal images for projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -169,3 +169,32 @@ for (i = 0; i < coll.length; i++) {
         }
     });
 }
+
+/*=============== PROJECTS MODAL ===============*/
+
+// create references to the modal...
+var workModal = document.getElementById('work__modal');
+// to all images -- note I'm using a class!
+var images = document.getElementsByClassName('work__img');
+// the image in the modal
+var modalImg = document.getElementById("img01");
+// and the caption in the modal
+var captionText = document.getElementById("caption");
+
+// Go through all of the images with our custom class
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  // and attach our click listener for this image.
+  img.onclick = function(evt) {
+    console.log(evt);
+    workModal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  workModal.style.display = "none";
+}
